cebd6cf8dc62505af1316fab2b9300fa4cb249ce
jhesterrr <jhesterrr@users.noreply.github.com>
2026-09-17 15:23:21 +0800

    Fix sidebar stickiness on desktop

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 export default function AppLayout({ children }: Props) {
   return (
-    <div className="flex w-full min-h-screen overflow-x-hidden relative">
+    <div className="flex w-full min-h-screen relative">
       <SidebarNav />
-      <div className="flex-1 flex flex-col min-w-0 w-full overflow-x-hidden">
+      <div className="flex-1 flex flex-col min-w-0 w-full">
         <HeaderBar />
         <main className="flex-1 w-full px-3.5 sm:px-4 md:px-6 py-4 md:py-6 pb-28 md:pb-6 max-w-[1600px] mx-auto overflow-x-hidden">
           {children}
